3956fb4cea42aff9482f7a9bbbeef8d252a19289
wessehkoffa <henrywslebo@gmail.com>
2017-02-03 13:54:36 -0500

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,9 @@
+
+
+
+   var person={ name: "Henry Slebo", age: 56, salary: 2000, city: "Newark", state: 
+   "New Jersey"};
+   console.log(" My name is " +  person.name +  "  and I am from "  +   person.state);
+                
+				
+				console.log(person.age);
